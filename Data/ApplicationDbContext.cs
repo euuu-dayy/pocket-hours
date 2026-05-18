13a3ca50ca4a30f29a71f104d40136ac0f6cbd2a
unknown <udaykaple5@gmail.com>
2026-05-18 19:41:39 +0530

    Fixed Railway MySQL table mapping

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,20 +1,53 @@
 ﻿using Microsoft.EntityFrameworkCore;
+
 using PocketHours.API.Models;
 
 namespace PocketHours.API.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext
+        : DbContext
     {
         public ApplicationDbContext(
-            DbContextOptions<ApplicationDbContext> options
+            DbContextOptions<ApplicationDbContext>
+                options
         ) : base(options)
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<User>
+            Users
+        { get; set; }
 
-        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Job>
+            Jobs
+        { get; set; }
 
-        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<JobApplication>
+            JobApplications
+        { get; set; }
+
+        protected override void
+            OnModelCreating(
+                ModelBuilder modelBuilder
+            )
+        {
+            base.OnModelCreating(
+                modelBuilder
+            );
+
+            modelBuilder
+                .Entity<User>()
+                .ToTable("users");
+
+            modelBuilder
+                .Entity<Job>()
+                .ToTable("jobs");
+
+            modelBuilder
+                .Entity<JobApplication>()
+                .ToTable(
+                    "jobapplications"
+                );
+        }
     }
 }
