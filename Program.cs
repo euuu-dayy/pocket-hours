774fd1a282f76d909788b449e8843f0284f5bece
unknown <udaykaple5@gmail.com>
2026-05-18 20:18:58 +0530

    Fixed fronted deployed url

--- Program.cs
+++ Program.cs
@@ -130,7 +130,8 @@ builder.Services.AddCors(options =>
             policy
                 .WithOrigins(
                     "http://localhost:5173",
-                    "https://localhost:5173"
+                    "https://localhost:5173",
+                    "https://pocket-hours-api.onrender.com/api"
                 )
                 .AllowAnyHeader()
                 .AllowAnyMethod();
